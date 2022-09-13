c8c49cb37d9a3383146cf5392885a4dc6b6e9c9c
RachieJosh <rachaeljosh23@gmail.com>
2022-09-13 14:40:47 -0700

    Task 1. Alphabet game cures insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - entry point
+  *
+  * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char g = 'a';
+
+	for (; g <= 'z'; g++)
+		_putchar(g);
+
+	_putchar('\n');
+}
