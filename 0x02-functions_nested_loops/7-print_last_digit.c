dee465df1f1e2cf0e33aaa2b41d6c862bd105543
RachieJosh <rachaeljosh23@gmail.com>
2022-09-14 13:52:26 -0700

    Task 7. Last digit printed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,6 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit;
-
-	last_digit = (n % 10);
-
-	if (last_digit < 0)
-	{
-		last_digit = (-n * last_digit);
-	}
-
-	_putchar(last_digit + '0');
-	return (last_digit);
+	_putchar('0' + _abs(n % 10));
+	return (_abs(n % 10));
 }
