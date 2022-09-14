31471cc48a31bff4fa7dbab51670d8512e52733c
RachieJosh <rachaeljosh23@gmail.com>
2022-09-14 12:50:30 -0700

    Task 8. Jack Bauer longest day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,6 +3,8 @@
 /**
   * jack_bauer - A function that prints every minute of Jack Bauer,
   *               starting from 00:00 to 23:59
+  *
+  * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
