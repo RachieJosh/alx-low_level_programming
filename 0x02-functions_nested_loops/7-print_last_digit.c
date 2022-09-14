95fd5858d940c63ce61bc5acd58ae3c943d03f26
RachieJosh <rachaeljosh23@gmail.com>
2022-09-13 21:06:02 -0700

    Task 7. Fixed an error

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,9 +10,13 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-		n = -n;
-	last_digit = n % 10;
+	last_digit = (n % 10);
+
+	if (last_digit < 0)
+	{
+		last_digit = (-n * last_digit);
+	}
+
 	_putchar(last_digit + '0');
 	return (last_digit);
 }
