d6852d26836acf33374a496349a1d074e20a8baf
RachieJosh <rachaeljosh23@gmail.com>
2022-09-13 19:42:25 -0700

    Task  2. Alphabet times ten

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,12 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char b = 'a';
-	char a = 0;
+	int b;
+	int a;
 
-	for (; a <= 9; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (; b >= 'z'; b++)
+		for (b =;'a'; b <= 'z'; b++)
 		{
 			_putchar(b);
 		}
