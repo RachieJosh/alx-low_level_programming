761c5a9c75cdd9ea778047630331ec89a0272d58
RachieJosh <rachaeljosh23@gmail.com>
2022-09-13 18:43:19 -0700

    Task 8. Federal agent, Jack Bauer longest day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * jack_bauer - A function that prints every minute of Jack Bauer,
+  *               starting from 00:00 to 23:59
+  *
+ */
+void jack_bauer(void)
+{
+	int min = 0;
+	int hour = 0;
+
+	for (; hour <= 23; hour++)
+	{
+		for (; min <= 59; min++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) = '0');
+			_putchar('\n');
+		}
+	}
+}
