5b6ba4c423881c8d8d3024fe6cca6de30c170dee
RachieJosh <rachaeljosh23@gmail.com>
2022-09-16 21:14:50 -0700

    Task 3. Numbers that speak up

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
 
 void print_numbers(void)
 {
-	int w;
+	int num;
 
-	for (w = 0; w <= 9; w++)
-		_putchar((w % 10) + 0);
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
 
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
-  * print_most_numbers - A function that prints the numbers 0 to 9, follwed by a new line
+  * print_most_numbers - A function that prints the numbers 0 to 9,
+  * followed by a new line
   *
   * Return: Always 0. (Success)
  */
 
 void print_most_numbers(void)
 {
-	int 
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' &&  i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
