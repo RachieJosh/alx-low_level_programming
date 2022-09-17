c7666350770dae35322b0eb233d034f0100466ef
RachieJosh <rachaeljosh23@gmail.com>
2022-09-16 19:45:27 -0700

    Task 0. Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * _isupper - A function that checks for uppercase character
+  *@c: Character to be checked
+  *
+  * Return: 1 if character is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * main - The header files for prototypes for this projects
+  *
+  * int prototypes
+  * void prototypes
+ */
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
