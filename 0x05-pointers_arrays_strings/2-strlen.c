45e073731cdf39a0b85336abbbcde5619c2866e4
RachieJosh <rachaeljosh23@gmail.com>
2022-09-19 16:55:40 -0700

    Task 1. Swapping horses is not advisable when racing

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: Character to be checked
+  *
+  * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	char *s;
+	int len;
+
+	len = strlen(*s);
+}
