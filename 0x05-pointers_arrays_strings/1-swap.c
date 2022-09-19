bbf936b434b82605afa3254dcac7fd68cc6e76d3
RachieJosh <rachaeljosh23@gmail.com>
2022-09-19 16:35:29 -0700

    Task 1. Swapping can't be done with horses in a race

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * swap_int - swaps the values of the two integers
+  * @a: Integer to be swapped
+  * @b: Integer to be swapped
+  *
+  * Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+	tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
