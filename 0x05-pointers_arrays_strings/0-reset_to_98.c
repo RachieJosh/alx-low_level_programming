da26dca467b408211a91aa9d55d49d1e63478d6d
RachieJosh <rachaeljosh23@gmail.com>
2022-09-19 15:58:32 -0700

    Task 0. 98 Battery street

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * reset_to_98 - Takes a pointer to an int as parameter,
+  *               and updates the value it points to to 98.
+  * @n: Pointer integer to be checked
+  *
+  * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	int *n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+  * main - The header files for prototypes for this projects
+  *
+  * void prototypes
+  * char prototype
+ */
+
+void reset_to_98(int *n)
+
+#endif /* MAIN_H */
