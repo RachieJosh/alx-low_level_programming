0a9e3ada90b860241bd24295db5c75f1ee73efcf
RachieJosh <rachaeljosh23@gmail.com>
2022-09-19 18:18:44 -0700

    Task 5. Reversed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * rev_string - It reverses a string
+  * @s: String to be checked
+  *
+  * Return: Always 0 (Success)
+ */
+
+void rev_string(char *s);
+{
+	int r;
+	char e;
+
+	for (r = 0; r < _strlen(s) / 2; r++)
+	{
+		e = s[r];
+		s[r] = s[_strlen(s) - r - 1];
+		s[_strlen(s) - r - 1] = e;
+	}
+}
