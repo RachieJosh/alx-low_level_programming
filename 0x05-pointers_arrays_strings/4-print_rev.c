27e07fc2eba80034d49a27a594ff792486dc9be1
RachieJosh <rachaeljosh23@gmail.com>
2022-09-19 17:58:26 -0700

    Task 4. Strings in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_rev - It prints a string in reverse
+  * @s: Character to be checked
+  *
+  * Return: Always 0 (Success)
+ */
+
+void print_rev(char *s)
+{
+	int z, y;
+
+	y = 0;
+	while (s[y] != '\0')
+		n++;
+	for (z = y - 1;  z >= 0; z--)
+	{
+		_putchar(s[z]);
+	}
+	_putchar('\n');
+}
