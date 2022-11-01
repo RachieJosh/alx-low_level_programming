f7a68790e09d46fa0d6a1323db5a5126b9f2aecb
RachieJosh <rachaeljosh23@gmail.com>
2022-11-01 08:44:19 -0700

    Had fun doing the Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ *
+ * Return: numbers
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
