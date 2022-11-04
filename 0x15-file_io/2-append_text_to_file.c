adb678ada276bd966f5ee8bdd9d5703c03bee745
RachieJosh <rachaeljosh23@gmail.com>
2022-11-04 16:31:39 -0700

    Corrected variable errors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,10 +16,10 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
+	if (text_content)
 	{
 		for (written = 0; text_content[written]; written++)
 		;
