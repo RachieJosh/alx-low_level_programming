a39260c66e55fea5770efe91bcd396f571810353
RachieJosh <rachaeljosh23@gmail.com>
2022-11-04 15:38:42 -0700

    hopefully cleared up the bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 	int written;
 	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		text_context = "";
+		text_content = "";
 	for (written = 0; text_content[written]; written++)
 		;
 	rwr = write(fd, text_content, written);
