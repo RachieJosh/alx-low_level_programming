e79f5b16b4e7de520f87e8d5e99421c37d1a760a
RachieJosh <rachaeljosh23@gmail.com>
2022-11-04 17:06:32 -0700

    Made the wrong corrections

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -10,8 +9,7 @@
 
 /**
   * main - The header files for the prototypes for this projects.
-  * @c: The character for putchar.
-  */
+ */
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
